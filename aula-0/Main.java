1038b1626e3d27d2975fbf13a0772949f3a2658d
Kauaan2k <kauanpra2009@gmail.com>
2025-05-16 15:02:41 -0300

    Update Main.java

--- aula-0/Main.java
+++ aula-0/Main.java
@@ -6,12 +6,11 @@ Write your code in this editor and press "Run" button to execute it.
 
 *******************************************************************************/
 import java.util.Scanner;
-public class Main
-{	public static void main(String[] args) 
-
+    public class Main {
+	public static void main(String[] args) {
 
    //Variáveis 1
-{
+ 
 	char name='K';
 	int year=15;
 	boolean breakfast=true;
@@ -19,26 +18,16 @@ public class Main
     System.out.println("my years "+ year);
 	System.out.println("breakfast "+ breakfast);
 	
-	//Variáveis 2
-
-	//int Laranja=6;
-	//int Uva=2;
-    //int result=Laranja+Uva;
-
 	
-    //Scanner ou Print
-
-    Scanner ka = new Scanner(System.in);
+	//Scanner
+	
+		Scanner ka = new Scanner(System.in);
     System.out.println("Laranja");
     int Laranja = ka.nextInt();
     
     System.out.println("Uva");
     int Uva = ka.nextInt();
     
-    System.out.print("resultado da soma = "+ (Laranja+Uva) );
-    
-
-	
+    System.out.print("resultado da soma = "+ (Laranja+Uva));
 	}
 }
-
