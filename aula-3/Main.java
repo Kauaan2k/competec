d6956a25bbda6036ca74211bae82e4eaf83156b4
Kauaan2k <kauanpra2009@gmail.com>
2025-05-24 11:32:05 -0300

    Update Main.java

--- aula-3/Main.java
+++ aula-3/Main.java
@@ -1,7 +1,39 @@
+import java.util.Scanner;
 public class Main
 {
+   
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
-		System.out.println("Hello World");
+	    Scanner ka = new Scanner (System.in);
+	
+	    
+        String letra = ka.nextLine();
+        int x = 1;
+        while  (x < 5) {
+            System.out.println(letra);
+            x++;
+        }
+        
+     
+     {System.out.println("resultado final");
+     }
+     
+        int k = 1;
+        int result = 0;
+        while (k <= 100) {
+            result += k;
+            k++;
+        }
+        
+        System.out.println(result);
+	                                      
+	                                      
+
+	    System.out.println("resultado");
+	    
+	    int tabuada = ka.nextInt();
+	    for (int i = 1; i <= 10; i++)
+	    {
+	    System.out.println(i*tabuada);
+	    }
 	}
 }
