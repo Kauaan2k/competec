8a1d8816605a8b54defb0b9463c64088ec3c8374
Kauaan2k <kauanpra2009@gmail.com>
2025-06-28 11:29:09 -0300

    Update Main.java

--- aula-5/Main.java
+++ aula-5/Main.java
@@ -1,7 +1,36 @@
+import java.util.Scanner;
 public class Main
 {
-	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
-		System.out.println("Hello World");
+	public static void main(String[] args)
+	{
+		{
+			Scanner Nome = new Scanner (System.in);
+
+
+			System.out.println("Nome do usuario");
+			String digite = Nome.nextLine();
+
+
+			int letras = digite.length();
+			System.out.println("ola, " +digite+ ", seu nome tem " +letras+ " caracteres.");
+		
+
+		
+		System.out.println("Qual a palavra secreta?");
+		String secreta = Nome.nextLine();
+		
+        boolean  con = (secreta.equalsIgnoreCase("batata"));
+        {
+	    if(con == true);
+	    System.out.println("Parábens você acertou");
+	    }
+	    	{
+             else if(con == false);
+        	System.out.println("você errou tente de novo");   
+	    	}
+	
+		}
+
 	}
+}
 }
