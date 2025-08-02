e74ed2753ee9e1b791368892ddbeafeb8c988d13
Kauaan2k <kauanpra2009@gmail.com>
2025-08-02 11:41:53 -0300

    Update Main.java

--- aula-6/Main.java
+++ aula-6/Main.java
@@ -1,7 +1,41 @@
 public class Main
 {
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
-		System.out.println("Hello World");
+		
+		
+		   
+	 {   //preço
+	        int papel = 10;
+            int copo = 5;
+            int chocolate = 7; 
+        
+        //quantidade
+	        int produto0 = 50;
+	        int produto1 = 20;
+	        int produto2 = 96;
+        
+        String estoque [] = {"papel", "copo", "chocolate"};
+        int valoresTotais [] = new int[3];
+	        
+	     System.out.println(estoque[0]);
+	     System.out.println("R$ "+papel);
+         System.out.println("estoque "+(produto0 * papel));
+         valoresTotais[0] = produto0*papel;
+         
+         System.out.println( );
+         System.out.println(estoque[1]);
+	     System.out.println("R$ "+copo);
+	     System.out.println("estoque "+(produto1 * copo));
+	     valoresTotais[1] = produto1*copo;
+	     
+	     System.out.println( );
+	     System.out.println(estoque[2]);
+	     System.out.println("R$ "+chocolate);
+	     System.out.println("estoque "+(produto2 * chocolate));
+	     valoresTotais[2] = produto2*chocolate;
+	   
+	   for maiorQuantidade = (0>1; )
+	 }
 	}
+		
 }
